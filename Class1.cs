20665cb2db6cdfd5d42d20bf067b21932a9a49bf
User <User@DESKTOP-NI>
2020-09-17 14:42:10 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Reflection;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swpublished;
+using SolidWorks.Interop.swconst;
+using SolidWorksTools;
+using SolidWorksTools.File;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.ComponentModel;
+
+namespace templateAddin
+{
+
+    [ComVisible(true)]
+    [Guid("9abb7e56-8685-4884-93e1-ad38d7071dc2")]
+    [DisplayName("PCB3D")]
+    [Description("PCB3D")]
+    
+    public class PCB3D : ISwAddin
+    {
+        #region Local Variables
+        ISldWorks iSwApp = null;
+        ICommandManager iCmdMgr = null;
+        int addinID = 0;
+     
+        public const int mainCmdGroupID = 5;
+        public const int mainItemID1 = 0;
+        #endregion
+
+        #region SolidWorks Registration
+        [ComRegisterFunctionAttribute]
+        public static void RegisterFunction(Type t)
+        {
+            #region Get Custom Attribute: SwAddinAttribute
+            SwAddinAttribute SWattr = null;
+            Type type = typeof(SwAddin);
+
+            foreach (System.Attribute attr in type.GetCustomAttributes(false))
+            {
+                if (attr is SwAddinAttribute)
+                {
+                    SWattr = attr as SwAddinAttribute;
+                    break;
+                }
+            }
+
+            #endregion
+
+            try
+            {
+                Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.Registry.LocalMachine;
+                Microsoft.Win32.RegistryKey hkcu = Microsoft.Win32.Registry.CurrentUser;
+
+                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + t.GUID.ToString() + "}";
+                Microsoft.Win32.RegistryKey addinkey = hklm.CreateSubKey(keyname);
+                addinkey.SetValue(null, 0);
+
+                addinkey.SetValue("Description", SWattr.Description);
+                addinkey.SetValue("Title", SWattr.Title);
+
+                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + t.GUID.ToString() + "}";
+                addinkey = hkcu.CreateSubKey(keyname);
+                addinkey.SetValue(null, Convert.ToInt32(SWattr.LoadAtStartup), Microsoft.Win32.RegistryValueKind.DWord);
+            }
+            catch (System.NullReferenceException nl)
+            {
+                Console.WriteLine("There was a problem registering this dll: SWattr is null. \n\"" + nl.Message + "\"");
+                System.Windows.Forms.MessageBox.Show("There was a problem registering this dll: SWattr is null.\n\"" + nl.Message + "\"");
+            }
+
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+                System.Windows.Forms.MessageBox.Show("There was a problem registering the function: \n\"" + e.Message + "\"");
+            }
+        }
+
+        [ComUnregisterFunctionAttribute]
+        public static void UnregisterFunction(Type t)
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey hklm = Microsoft.Win32.Registry.LocalMachine;
+                Microsoft.Win32.RegistryKey hkcu = Microsoft.Win32.Registry.CurrentUser;
+
+                string keyname = "SOFTWARE\\SolidWorks\\Addins\\{" + t.GUID.ToString() + "}";
+                hklm.DeleteSubKey(keyname);
+
+                keyname = "Software\\SolidWorks\\AddInsStartup\\{" + t.GUID.ToString() + "}";
+                hkcu.DeleteSubKey(keyname);
+            }
+            catch (System.NullReferenceException nl)
+            {
+                Console.WriteLine("There was a problem unregistering this dll: " + nl.Message);
+                System.Windows.Forms.MessageBox.Show("There was a problem unregistering this dll: \n\"" + nl.Message + "\"");
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine("There was a problem unregistering this dll: " + e.Message);
+                System.Windows.Forms.MessageBox.Show("There was a problem unregistering this dll: \n\"" + e.Message + "\"");
+            }
+        }
+
+        #endregion
+
+        #region ISwAddin Implementation
+
+        public bool ConnectToSW(object ThisSW, int cookie)
+        {
+            iSwApp = ThisSW as ISldWorks;
+            addinID = cookie;
+
+            //Setup callbacks
+            iSwApp.SetAddinCallbackInfo(0, this, addinID);
+
+            #region Setup the Command Manager
+            iCmdMgr = iSwApp.GetCommandManager(cookie);
+            AddCommandMgr();
+            #endregion
+
+            return true;
+        }
+
+        public bool DisconnectFromSW()
+        {
+            RemoveCommandMgr();
+ 
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(iCmdMgr);
+            iCmdMgr = null;
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(iSwApp);
+            iSwApp = null;
+            //The addin _must_ call GC.Collect() here in order to retrieve all managed code pointers 
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            return true;
+        }
+        #endregion
+
+        #region UI Methods
+        public void AddCommandMgr()
+        {
+            ICommandGroup cmdGroup;
+
+            int cmdIndex0;
+            string Title = "Addin", ToolTip = "Addin";
+
+            int cmdGroupErr = 0;
+            bool ignorePrevious = false;
+
+            object registryIDs;
+            //get the ID information stored in the registry
+            bool getDataResult = iCmdMgr.GetGroupDataFromRegistry(mainCmdGroupID, out registryIDs);
+
+            int[] knownIDs = new int[1] { mainItemID1};
+
+            if (getDataResult)
+            {
+                if (!CompareIDs((int[])registryIDs, knownIDs)) //if the IDs don't match, reset the commandGroup
+                {
+                    ignorePrevious = true;
+                }
+            }
+
+            cmdGroup = iCmdMgr.CreateCommandGroup2(mainCmdGroupID, Title, ToolTip, "Аддин", -1, ignorePrevious, ref cmdGroupErr);
+         
+            int menuToolbarOption = (int)(swCommandItemType_e.swMenuItem | swCommandItemType_e.swToolbarItem);
+            cmdIndex0 = cmdGroup.AddCommandItem2("Create3D", -1, "Create a 3D", "Create 3D", 0, "Create3D", "", mainItemID1, menuToolbarOption);
+           
+            cmdGroup.HasToolbar = true;
+            cmdGroup.HasMenu = true;
+            cmdGroup.Activate();
+        }
+
+        public void RemoveCommandMgr()
+        {
+            iCmdMgr.RemoveCommandGroup(mainCmdGroupID);
+        }
+
+        public bool CompareIDs(int[] storedIDs, int[] addinIDs)
+        {
+            List<int> storedList = new List<int>(storedIDs);
+            List<int> addinList = new List<int>(addinIDs);
+
+            addinList.Sort();
+            storedList.Sort();
+
+            if (addinList.Count != storedList.Count)
+            {
+                return false;
+            }
+            else
+            {
+
+                for (int i = 0; i < addinList.Count; i++)
+                {
+                    if (addinList[i] != storedList[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        #endregion
+
+        #region UI Callbacks
+        public void Create3D()
+        {
+            iSwApp.SendMsgToUser("Hello World!");
+        }
+
+        #endregion
+
+    }
+
+
+}
+
